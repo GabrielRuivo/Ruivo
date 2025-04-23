75afb20442a7af32a9b25b41c328a5d2fc308839
GabrielRuivo <gabriel-ruivo@hotmail.com>
2025-04-23 18:47:26 -0300

    feat: update Services component with new experience details and header comment
    - Added new experience entry for "Smart FIIs" with detailed descriptions in both Portuguese and English.
    - Updated existing experience entries with website links for "Férias & Co." and "Bankme".
    - Commented out the Header component in the Home page for potential future use.
    </message>
    
    <message>feat: atualizar componente Services com novos detalhes de experiências e comentário no cabeçalho
    
    - Adicionada nova entrada de experiência para "Smart FIIs" com descrições detalhadas em português e inglês.
    - Atualizadas as entradas de experiência existentes com links de sites para "Férias & Co." e "Bankme".
    - Comentado o componente Header na página Home para uso potencial futuro.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 "use client"; // this is a client component 👈🏽
 import Experiences from "@/components/Experiences/Experiences";
-import Header from "@/components/Header/Header";
+// import Header from "@/components/Header/Header";
 import Land from "@/pages/Land/Land";
 import Services from "@/pages/Services/Services";
 
 export default function Home() {
   return (
     <div className="overflow-hidden">
-      <Header />
+      {/* <Header /> */}
       <Land />
       <Experiences />
       <Services />
