99d0f9c828a71c61e664151400ae0b58f368b1da
GabrielRuivo <gabriel-ruivo@hotmail.com>
2025-04-24 09:57:28 -0300

    feat: add favicon, manifest, and icon files for portfolio
    - Updated favicon.ico for improved branding.
    - Added manifest.json to define the web app's metadata and icons.
    - Introduced icon.svg for enhanced visual representation in the app.
    - Modified Head component to include new favicon and manifest links.
    </message>
    
    <message>feat: adicionar arquivos de favicon, manifest e ícone para o portfólio
    
    - Atualizado favicon.ico para melhorar a identidade visual.
    - Adicionado manifest.json para definir os metadados e ícones do aplicativo web.
    - Introduzido icon.svg para uma representação visual aprimorada no aplicativo.
    - Modificado o componente Head para incluir novos links de favicon e manifest.

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -4,7 +4,11 @@ export default function Head() {
       <title>Ruivo's Portfolio</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Ruivo's Portfolio" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <link rel="icon" href="/icons/icon.svg" type="image/svg+xml" />
+      <link rel="apple-touch-icon" href="/favicon.ico" />
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="theme-color" content="#FF5F6D" />
     </>
   )
 }
